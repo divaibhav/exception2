0cd59325aa711020296e5c49fffe4d9149db2ed9
divaibhav <divaibhav@gmail.com>
2019-09-25 17:51:54 +0530

    created ArrayMain class with main method and search method. created skeleton for main() and search().

---
+++ src/main/ArrayMain.java
@@ -0,0 +1,15 @@
+package main;
+
+import exception.NoElementFoundException;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+
+    }
+    public boolean search(int[] arr, int searchElement) throws NoElementFoundException {
+        boolean response = false;
+
+        return response;
+
+    }
+}
