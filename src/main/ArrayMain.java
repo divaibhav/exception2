4432087b20a265ad8664b6d4f610e6c326d7cd4d
divaibhav <divaibhav@gmail.com>
2019-09-25 17:56:33 +0530

    implemented mainn method

--- src/main/ArrayMain.java
+++ src/main/ArrayMain.java
@@ -4,6 +4,17 @@ import exception.NoElementFoundException;
 
 public class ArrayMain {
     public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+        int searchElement = 150;
+
+        ArrayMain obj = new ArrayMain();
+        try {
+            if(obj.search(arr, searchElement)){
+                System.out.println("element found");
+            }
+        } catch (NoElementFoundException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
     public boolean search(int[] arr, int searchElement) throws NoElementFoundException {
